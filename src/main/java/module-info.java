7ee7983879cd7e17ee587a6c38730c496e1eeff3
BENHDOUBA Ziyad <benhdouba.ziyad@gmail.com>
2025-06-30 19:31:15 +0100

    project init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module ma.ensi.ensidesktopapp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens ma.ensi.ensidesktopapp to javafx.fxml;
+    exports ma.ensi.ensidesktopapp;
+}
