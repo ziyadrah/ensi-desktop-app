901215f0de63fcca337ae1f7d066ff8b53e56fe5
BENHDOUBA Ziyad <benhdouba.ziyad@gmail.com>
2025-07-01 13:23:24 +0100

    bigger dimension

--- src/main/java/ma/ensi/ensidesktopapp/HelloApplication.java
+++ src/main/java/ma/ensi/ensidesktopapp/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 //        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("eleve.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
