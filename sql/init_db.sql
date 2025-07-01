5ff7b58765dd1f7d3e13e9acf0efe4f1454d2666
BENHDOUBA Ziyad <benhdouba.ziyad@gmail.com>
2025-07-01 05:47:10 +0100

    added users table for future authentication

--- sql/init_db.sql
+++ sql/init_db.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Filiere (
+CREATE TABLE IF NOT EXISTS Filiere (
     id SERIAL PRIMARY KEY,
     code VARCHAR(10) UNIQUE NOT NULL,
     designation VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE Module (
+CREATE TABLE IF NOT EXISTS Module (
     id SERIAL PRIMARY KEY,
     code VARCHAR(10) UNIQUE NOT NULL,
     designation VARCHAR(100) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE Module (
     code_filiere INTEGER REFERENCES Filiere(id)
 );
 
-CREATE TABLE Matiere (
+CREATE TABLE IF NOT EXISTS Matiere (
     id SERIAL PRIMARY KEY,
     code VARCHAR(10) UNIQUE NOT NULL,
     designation VARCHAR(100) NOT NULL,
@@ -21,7 +21,7 @@ CREATE TABLE Matiere (
     code_module INTEGER REFERENCES Module(id)
 );
 
-CREATE TABLE Eleve (
+CREATE TABLE IF NOT EXISTS Eleve (
     id SERIAL PRIMARY KEY,
     code VARCHAR(10) UNIQUE NOT NULL,
     nom VARCHAR(100) NOT NULL,
@@ -30,17 +30,28 @@ CREATE TABLE Eleve (
     code_filiere INTEGER REFERENCES Filiere(id)
 );
 
-CREATE TABLE Notes (
+CREATE TABLE IF NOT EXISTS Notes (
     id SERIAL PRIMARY KEY,
     code_eleve INTEGER REFERENCES Eleve(id),
     code_matiere INTEGER REFERENCES Matiere(id),
     note NUMERIC(4,2) CHECK (note >= 0 AND note <= 20)
 );
 
-CREATE TABLE Moyennes (
+CREATE TABLE IF NOT EXISTS Moyennes (
     id SERIAL PRIMARY KEY,
     code_eleve INTEGER REFERENCES Eleve(id),
     code_filiere INTEGER REFERENCES Filiere(id),
     niveau INTEGER NOT NULL,
     moyenne NUMERIC(4,2)
 );
+
+CREATE TABLE IF NOT EXISTS Users (
+    id    SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    password TEXT NOT NULL
+);
+
+INSERT INTO Users (email, nom, prenom, password)
+VALUES ('benhdouba.ziyad@gmail.com', 'Benhdouba', 'Ziyad', '');
